c036339ba4ae2a3c72b6c4a9374a73161eb4d4fc
ZeynelNes <ulusoyzeynel00@gmail.com>
2025-05-16 00:37:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,45 @@ const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
 
 // Müzik listesi
+
+// const container = document.querySelector('.container');
+
+const backgrounds = [
+  'background1.jpg',
+  'background2.jpg',
+  'background3.jpg',
+  'background4.jpg',
+  'background5.jpg',
+  'background6.jpg',
+  'background7.jpg',
+  'background8.jpg',
+  'background9.jpg',
+  'background10.jpg',
+  'background11.jpg',
+  'background12.jpg',
+  'background13.jpg',
+  'background14.jpg',
+  'background15.jpg',
+  'background16.jpg',
+  'background17.jpg',
+  'background18.jpg',
+  'background19.jpg'
+  
+];
+
+let currentBgIndex = 0;
+
+function changeBackground() {
+  container.style.backgroundImage = `url(${backgrounds[currentBgIndex]})`;
+  currentBgIndex = (currentBgIndex + 1) % backgrounds.length;
+}
+
+// İlk arka planı ayarla
+changeBackground();
+
+// Her 5 saniyede arka planı değiştir
+setInterval(changeBackground, 5000);
+//
 const musicList = ['music1.mp3', 'music2.mp3', 'music3.mp3'];
 let currentMusicIndex = 0;
 
@@ -64,41 +103,4 @@ toggleVoiceBtn.addEventListener('click', () => {
   }
 });
 
-const container = document.querySelector('.container');
-
-const backgrounds = [
-  'background1.jpg',
-  'background2.jpg',
-  'background3.jpg',
-  'background4.jpg',
-  'background5.jpg',
-  'background6.jpg',
-  'background7.jpg',
-  'background8.jpg',
-  'background9.jpg',
-  'background10.jpg',
-  'background11.jpg',
-  'background12.jpg',
-  'background13.jpg',
-  'background14.jpg',
-  'background15.jpg',
-  'background16.jpg',
-  'background17.jpg',
-  'background18.jpg',
-  'background19.jpg',
-  
-];
-
-let currentBgIndex = 0;
-
-function changeBackground() {
-  container.style.backgroundImage = `url(${backgrounds[currentBgIndex]})`;
-  currentBgIndex = (currentBgIndex + 1) % backgrounds.length;
-}
-
-// İlk arka planı ayarla
-changeBackground();
-
-// Her 5 saniyede arka planı değiştir
-setInterval(changeBackground, 5000);
 
